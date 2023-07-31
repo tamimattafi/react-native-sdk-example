4107f6511e5954ab7d1447657f86f24e6af07000
Tamim Attafi <tamim.attafi@i-link.pro>
2023-07-31 22:23:43 +0700

    config: add ios sample

---
+++ sample/ios/RNSDKSample/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  RNSDKSample
+//
+//  Created by Tamim Attafi on 31.07.2023.
+//
+
+import UIKit
+import RNSDK
+
+class ViewController: RNSDKViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
